01249cfa9973bf3f5827e0bf73997433d9ed04ea
selubi <gregorius.bryan@selubi.tech>
2026-07-24 02:35:57 +0900

    cleanup

--- modules/home/programs/1password.nix
+++
@@ -1,9 +0,0 @@
-# 1password.nix
-#
-{ machineConfig, ... }: {
-
-  home.packages = [
-    pkgs._1password
-    pkgs._1password-gui
-  ];
-}
--- modules/home/programs/google-chrome.nix
+++ modules/home/programs/google-chrome.nix
@@ -1,4 +1,6 @@
 # google-chrome.nix
+# If you search this via option search, it won't pop up.
+# Its actually programs.chromium but enabled via dynamic attribute at https://github.com/nix-community/home-manager/blob/3b0e6bbd65869af1beadf5963a99befc179d209f/modules/programs/chromium.nix#L341-L351
 { ... }:
 {
   programs.google-chrome = {
