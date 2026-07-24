729cb8b5a4dc7acc1899338303f7040d4d1dd198
selubi <gregorius.bryan@selubi.tech>
2026-07-25 00:22:57 +0900

    comments

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -7,6 +7,7 @@
     # This can work if the only profile available is the default profile.
     # e.g. Do not define profile.<name> other than profile.default
     # The compile warning is more correct `warning: programs.vscode.mutableExtensionsDir can be used only if no profiles apart from default are set.`
+    # Keep this true for sanity, or you need to change nix settings everytime installing an extension
     mutableExtensionsDir = true;
 
     profiles.default = {
