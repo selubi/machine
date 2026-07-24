33adaae51f1e042ead33266beb1b4b1e93187266
selubi <gregorius.bryan@selubi.tech>
2026-07-25 00:32:21 +0900

    move core config to its own

--- home/users/selubi/default.nix
+++ home/users/selubi/default.nix
@@ -1,22 +1,8 @@
+{ pkgs, ... }:
 {
-  pkgs,
-  nixContext,
-  userConfig,
-  machineConfig,
-  ...
-}:
-{
-
-  home.username = userConfig.userName;
-  home.homeDirectory =
-    if machineConfig.isDarwin then "/Users/${userConfig.userName}" else "/home/${userConfig.userName}";
-  targets.genericLinux.enable = machineConfig.isLinux && !machineConfig.isNixOS;
-  # NEVER CHANGE THIS AFTER THE INITIAL INSTALLATION UNLESS YOU KNOW WHAT YOU ARE DOING!
-  # If you ever want to change this, you would need to wipe home manager on every machine and re-install it.
-  home.stateVersion = "26.05";
-  nixpkgs.config.allowUnfree = true;
 
   imports = [
+    ../../core
     ../../features/cli.nix
     ../../features/machine-update.nix
     ../../features/browser.nix
@@ -37,12 +23,5 @@
     # Custom packages
     # (callPackage ../../pkgs/hms { })
   ];
-
-  # Needs reboot after applying
-  home.sessionVariables = {
-    NXM_FLAKE = nixContext.flakeRef;
-    NXM_TARGET = nixContext.targetName;
-    NXM_SYSTEM = machineConfig.system;
-  };
 
 }
