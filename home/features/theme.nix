d1909de9d56152b7951ee39c7fcd078f111760cf
selubi <gregorius.bryan@selubi.tech>
2026-08-01 01:00:53 +0900

    fcitx5 enable rounded

--- home/features/theme.nix
+++ home/features/theme.nix
@@ -1,17 +1,22 @@
+# features/theme.nix
 { ... }:
 {
   # https://nix.catppuccin.com/
+  # Basically we control all application looks here.
+  # Compared to defining the themes at each app, this will make things easier to cleanup.
+
   catppuccin = {
     enable = true;
-    autoEnable = false;
+    autoEnable = true;
 
-    btop.enable = true;
-    eza.enable = true;
-    fish.enable = true;
-    vscode.profiles.default = {
-      enable = true;
-      icons.enable = true;
-    };
+    # We only need to enable things manually that is not enabled by catppuccin.autoEnable
+    # We can search for what those are by searching '"Default: false"' in https://nix.catppuccin.com/
+    cursors.enable = true;
+    fcitx5.enableRounded = true;
   };
+
+  # As for now, only catppuccin uses these (i.e. the only modification we do is aesthethic)
+  # When there are more settings that depends on them, move to standalone module
+  home.pointerCursor.enable = true;
 
 }
