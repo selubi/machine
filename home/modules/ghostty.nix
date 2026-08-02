20959addaed1f2a3400ea323ca373829d0b5b229
selubi <gregorius.bryan@selubi.tech>
2026-08-02 16:53:48 +0900

    ghostty

--- home/modules/ghostty.nix
+++ home/modules/ghostty.nix
@@ -1,8 +1,11 @@
 # home/modules/ghostty.nix
-{ ... }:
+
+# Somehow needs restart or you might get desktop errors when launching even though from cli its fine
+{ machineConfig, ... }:
 {
   programs.ghostty = {
     enable = true;
     enableFishIntegration = true;
+    systemd.enable = machineConfig.isLinux;
   };
 }
